c6de4c9247d243c1c78bee5f1049285f792d55db
Sevrinn <sevrinnsevrinn@gmail.com>
2021-08-26 12:27:53 -0700

    added additional personCards with different information passed down from props in each

--- src/App.js
+++ src/App.js
@@ -10,6 +10,24 @@ function App() {
         age={35}
         hairColor={"Light Brown"}
       />
+      <PersonCard
+        firstName={"Greg"}
+        lastName={"Yang"}
+        age={33}
+        hairColor={"Black"}
+      />
+      <PersonCard
+        firstName={"Mike"}
+        lastName={"Winetroub"}
+        age={38}
+        hairColor={"Blond"}
+      />
+      <PersonCard
+        firstName={"Ash"}
+        lastName={"Ketchum"}
+        age={11}
+        hairColor={"Black"}
+      />
     </div>
   );
 }
