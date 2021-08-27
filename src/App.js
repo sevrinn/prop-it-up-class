37980c31e944c6a0626f9b9eef9983d80e1aba5d
Sevrinn <sevrinnsevrinn@gmail.com>
2021-08-27 12:33:27 -0700

    made app a class component and commented out unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,35 +1,52 @@
+import React, { Component } from "react";
+
 import PersonCard from "./components/PersonCard";
+import MyNewComponent from "./components/MyNewComponent";
+import LightSwitch from "./components/LightSwitch";
+
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <PersonCard
-        firstName={"James"}
-        lastName={"Arnold"}
-        age={35}
-        hairColor={"Light Brown"}
-      />
-      <PersonCard
-        firstName={"Greg"}
-        lastName={"Yang"}
-        age={33}
-        hairColor={"Black"}
-      />
-      <PersonCard
-        firstName={"Mike"}
-        lastName={"Winetroub"}
-        age={38}
-        hairColor={"Blond"}
-      />
-      <PersonCard
-        firstName={"Ash"}
-        lastName={"Ketchum"}
-        age={11}
-        hairColor={"Black"}
-      />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <PersonCard
+          firstName={"James"}
+          lastName={"Arnold"}
+          age={34}
+          hairColor={"Light Brown"}
+        />
+
+        <PersonCard
+          firstName={"Greg"}
+          lastName={"Yang"}
+          age={33}
+          hairColor={"Black"}
+        />
+
+        <PersonCard
+          firstName={"Mike"}
+          lastName={"Winetroub"}
+          age={38}
+          hairColor={"Blond"}
+        />
+
+        <PersonCard
+          firstName={"Ash"}
+          lastName={"Ketchum"}
+          age={11}
+          hairColor={"Black"}
+        />
+
+        <MyNewComponent header={"What's up?!"}>
+          <p>This is a child</p>
+          <p>This is another child</p>
+          <p>This is even another child</p>
+        </MyNewComponent>
+        <LightSwitch />
+      </div>
+    );
+  }
 }
 
 export default App;
